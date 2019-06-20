f340faa076cf1241adbc01728e43022612580306
danutzzb <45382669+danutzzb@users.noreply.github.com>
2019-06-20 18:36:39 +0300

    Pass works without hash

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_NAME', 'proiectPhp');
+define('DB_NAME', 'proiectphp');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_HOST', 'localhost');
@@ -7,20 +7,26 @@ define('DB_HOST', 'localhost');
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 function User(){
-    
-    if(isset($_POST['uname']) && isset($_POST['psw'])){
 
-        $user = filter_var($_POST['uname'],FILTER_SANITIZE_STRING);
-        $pass = password_hash($_POST['psw'], PASSWORD_DEFAULT);
-        $pass = filter_var($pass, FILTER_SANITIZE_STRING);
+    if(isset($_POST['uname']) && isset($_POST['psw'])){
+    
+   
+
+        $user = filter_var($_POST['email'],FILTER_SANITIZE_EMAIL);
+        $pass = filter_var($_POST['psw'], FILTER_SANITIZE_STRING);
+        $pass = password_hash($pass, PASSWORD_DEFAULT);
         
-        echo $user . "<br>";
-        echo $pass . "<br>";
+ 
         
+        $add = "INSERT INTO users(email,password)VALUES('$user','$pass')";
+        
+       
+       echo 'User succesfuly added to database';
     }
       else {       
         echo 'error';
 }  
-    
+     
+  
 }
  
--- db.php
+++ db.php
@@ -3,13 +3,20 @@ require_once 'config.php';
 
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-User();
+$user = filter_var($_POST['email'],FILTER_SANITIZE_EMAIL);
 
+$pass = filter_var($_POST['psw'], FILTER_SANITIZE_STRING);
+$psw = password_hash($pass, PASSWORD_DEFAULT);
+
+echo $user;
+echo $pass;
+
+$add ="INSERT INTO users(email,password)VALUES('$user','$psw')";
+mysqli_query($conn, $add);
+
+
+ 
 
         
       
-  
-  
-  
-
-
+    
