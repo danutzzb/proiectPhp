30e8eb1091ee399e41da71f2d049ba457657da2d
danutzzb <45382669+danutzzb@users.noreply.github.com>
2019-06-21 19:25:31 +0300

    Signup Functional

--- db.php
+++ db.php
@@ -7,7 +7,6 @@ $pass = mysqli_real_escape_string($conn,$_POST['psw']);
 $psw = hash('sha256', $pass);
 
 $add = "INSERT INTO users(email,password)VALUES('$user', '$psw')";
-
 if(mysqli_query($conn, $add)){
     
     echo 'User succesfully added to database';
